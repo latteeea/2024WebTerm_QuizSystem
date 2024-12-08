ceb26532e4afd55cc0e88398588c82a1a8e035ca
latteeea <kimtaeran4767@gmail.com>
2024-12-09 04:25:55 +0900

    quizSetting

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const session = require('express-session');
 const authRoutes = require('./routes/authRoutes');
 const nunjucks = require('nunjucks');
 const path = require('path');
+const quizRoutes = require('./routes/quizRoutes');
+const fs = require('fs');
+
+
 
 const app = express();
 
@@ -17,17 +21,35 @@ app.set('view engine', 'html');
 
 // Middleware 설정
 app.use(express.urlencoded({ extended: false }));
+
 app.use(session({
     secret: 'your_secret_key',
     resave: false,
     saveUninitialized: true,
 }));
 
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+});
+
+
 // 정적 파일 경로
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 라우트 등록
 app.use('/', authRoutes);
+
+app.use('/quiz', quizRoutes);
+
+app.get('/dashboard', (req, res) => {
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+    res.render('dashboard', { user: req.session.user });
+});
+
+
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Welcome to the Quiz System!' });
--- db.js
+++ db.js
@@ -8,6 +8,8 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+
 });
+
 
 module.exports = pool;
